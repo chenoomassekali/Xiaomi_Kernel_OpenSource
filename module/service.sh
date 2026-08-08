0154dba8ea5fcf05da8364eb0f1ff6c4c55a2fc0
chenoomassekali <chenoomas@gmail.com>
2026-08-08 17:39:30 +0900

    Create service.sh

---
+++ module/service.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+PKG="app.ninjavpn.android"
+
+while [ "$(getprop sys.boot_completed)" != "1" ]; do sleep 1; done
+sleep 5
+
+UID_NUM=$(dumpsys package $PKG | grep -m1 "userId=" | sed 's/.*userId=//')
+
+if [ -n "$UID_NUM" ]; then
+  cmd netpolicy add-restrict-background-whitelist $UID_NUM
+  cmd netpolicy remove-restrict-background-blacklist $UID_NUM
+  cmd netpolicy set-uid-policy $UID_NUM 0
+  dumpsys deviceidle whitelist +$PKG
+  cmd appops set $PKG RUN_IN_BACKGROUND allow
+  cmd appops set $PKG RUN_ANY_IN_BACKGROUND allow
+fi
+
+cmd netpolicy set-restrict-background false
+settings put global data_saver_mode 0
+
+log -t unrestrict_ninjavpn "Applied unrestrict policy for $PKG (uid=$UID_NUM)"
