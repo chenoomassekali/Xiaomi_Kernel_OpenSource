aa11623a9a95b59613072d2bd0645bad9f0f978a
chenoomassekali <chenoomas@gmail.com>
2026-08-08 18:23:19 +0900

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -7,15 +7,14 @@ sleep 5
 UID_NUM=$(dumpsys package $PKG | grep -m1 "userId=" | sed 's/.*userId=//')
 
 if [ -n "$UID_NUM" ]; then
-  cmd netpolicy add-restrict-background-whitelist $UID_NUM
-  cmd netpolicy remove-restrict-background-blacklist $UID_NUM
-  cmd netpolicy set-uid-policy $UID_NUM 0
+  cmd netpolicy add restrict-background-whitelist $UID_NUM
+  cmd netpolicy remove restrict-background-blacklist $UID_NUM
   dumpsys deviceidle whitelist +$PKG
   cmd appops set $PKG RUN_IN_BACKGROUND allow
   cmd appops set $PKG RUN_ANY_IN_BACKGROUND allow
 fi
 
-cmd netpolicy set-restrict-background false
+cmd netpolicy set restrict-background false
 settings put global data_saver_mode 0
 
 log -t unrestrict_ninjavpn "Applied unrestrict policy for $PKG (uid=$UID_NUM)"
